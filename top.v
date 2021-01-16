fc9c40c68a8aeb6590bef92f332453dc8c8d5733
David Jander <djander@gmail.com>
2021-01-16 16:16:54 +0100

    video.v: Implement simple write-only video text buffer
    Signed-off-by: David Jander <djander@gmail.com>

--- top.v
+++ top.v
@@ -23,6 +23,9 @@ module top(
 	wire [7:0] bnksel;
 	wire [9:0] raddr;
 	wire [3:0] unused_gpdi_dn;
+	wire video_stb;
+	wire [31:0] video_dat_o;
+	wire video_ack;
 
 	initial $readmemh("firmware.hex", ram);
 
@@ -39,6 +42,8 @@ module top(
 
 	assign led = led_reg;
 
+	assign video_stb = (bnksel == 8'h02) & stb_o;
+
 	cpu cpu(
 		.clk(clk_25mhz),
 		.rst_i(reset),
@@ -54,6 +59,13 @@ module top(
 
 	video video(
 		.clk_25mhz(clk_25mhz),
+		.adr_i(adr_o),
+		.dat_i(dat_o),
+		.sel_i(sel_o),
+		.we_i(we_o),
+		.stb_i(video_stb),
+		.ack_o(video_ack),
+		.dat_o(video_dat_o),
 		.gpdi_dp(gpdi_dp),
 		.gpdi_dn(unused_gpdi_dn)
 	);
@@ -91,7 +103,9 @@ module top(
 						dat_i[23:16] <= sel_o[2] ? ram[raddr][23:16] : 8'h00;
 						dat_i[31:24] <= sel_o[0] ? ram[raddr][31:24] : 8'h00;
 					end
-					default: dat_i <= 32'h00000000;
+					8'h02: dat_i <= video_dat_o;
+					default: begin
+					end
 				endcase
 			end
 		end
--- video.v
+++ video.v
@@ -1,13 +1,50 @@
 
 module video(
 	input clk_25mhz,
+	input [12:0] adr_i,
+	input [31:0] dat_i,
+	input [3:0] sel_i,
+	input we_i,
+	input stb_i,
+	output ack_o,
+	output reg [31:0] dat_o,
 	output [3:0] gpdi_dp, gpdi_dn
 );
+	reg [7:0] chargen[0:4095];
+	reg [7:0] ram[0:4799];
 	wire [23:0] color;
 	wire [9:0] x;
 	wire [9:0] y;
+	wire [8:0] cidx;
+	wire [2:0] cx;
+	wire [2:0] cy;
+	wire [11:0] cgenaddr;
+	wire [6:0] col;
+	wire [5:0] row;
+	wire [12:0] ramidx;
+	reg write;
+	reg [7:0] wdata;
+	reg [12:0] waddr;
 
-	assign color = x[4] ^ y[4] ? 24'hffffff : {x[7:0], y[7:0], x[9:2]};
+	assign col = x[9:3];
+	assign row = y[8:3];
+	assign ramidx = {row, 6'b000000} + {2'b00, row, 4'b0000} + {5'b00000, col};
+	assign cidx = {1'b1, ram[ramidx[12:0]]};
+	assign cx = 7 - x[2:0];
+	assign cy = y[2:0];
+	assign cgenaddr = {cidx, cy};
+	assign ack_o = stb_i;
+
+	integer i;
+
+	initial begin
+		$readmemh("chargen.hex", chargen);
+		$readmemh("screenram.hex", ram);
+		for (i = 24; i < 4800; i = i + 1)
+			ram[i] = 8'h00;
+	end
+
+	assign color = chargen[cgenaddr][cx] ? 24'hffffff : 24'h000000;
 
 	hdmi_video hdmi_video
 	(
@@ -18,4 +55,22 @@ module video(
 		.gpdi_dp(gpdi_dp),
 		.gpdi_dn(gpdi_dn)
 	);
+
+	always @(posedge clk_25mhz) begin
+		if (write) begin
+			ram[waddr] <= wdata;
+		end
+	end
+
+	always @(posedge clk_25mhz) begin
+		if (stb_i) begin
+			if (we_i & sel_i[0]) begin
+				wdata <= dat_i[7:0];
+				waddr <= adr_i;
+				write <= 1;
+			end else begin
+				write <= 0;
+			end
+		end
+	end
 endmodule
