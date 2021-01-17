6aa0bb475a45ca5339a3f589226a9348b8683778
David Jander <djander@gmail.com>
2021-01-17 13:10:43 +0100

    video.v: Simplify ram access and eliminate state machine
    Signed-off-by: David Jander <djander@gmail.com>

--- video.v
+++ video.v
@@ -7,7 +7,7 @@ module video(
 	input [3:0] sel_i,
 	input we_i,
 	input stb_i,
-	output reg ack_o,
+	output ack_o,
 	output reg [31:0] dat_o,
 	output [3:0] gpdi_dp, gpdi_dn
 );
@@ -24,10 +24,6 @@ module video(
 	wire [6:0] col;
 	wire [5:0] row;
 	wire [12:0] ramidx;
-	reg write, read;
-	reg [7:0] wdata;
-	reg [12:0] waddr, raddr;
-	reg [2:0] state;
 
 	assign col = x[9:3];
 	assign row = y[8:3];
@@ -36,15 +32,12 @@ module video(
 	assign cx = 7 - x[2:0];
 	assign cy = y[2:0];
 	assign cgenaddr = {cidx, cy};
+	assign ack_o = stb_i;
 
 	integer i;
 
 	initial begin
 		$readmemh("chargen.hex", chargen);
-		for (i = 0; i < 4800; i = i + 1) begin
-			ram0[i] = 8'h00;
-			ram1[i] = 8'h00;
-		end
 	end
 
 	assign color = chargen[cgenaddr][cx] ? 24'hffffff : 24'h000000;
@@ -60,48 +53,13 @@ module video(
 	);
 
 	always @(posedge clk_25mhz) begin
-		if (rst_i) begin
-			state <= 3'b000;
-		end else begin
-			case (state)
-				3'b000: begin
-					if (stb_i) begin
-						if (we_i & sel_i[0]) begin
-							wdata <= dat_i[7:0];
-							waddr <= adr_i;
-							ack_o <= 1;
-							state <= 3'b001;
-						end else if (sel_i[0]) begin
-							raddr <= adr_i;
-							state <= 3'b100;
-						end
-					end
-				end
-				3'b001: begin
-					ram0[waddr] <= wdata;
-					ram1[waddr] <= wdata;
-					state <= 3'b010;
-				end
-				3'b010: begin
-					ack_o <= 0;
-					state <= 3'b000;
-				end
-				3'b100: begin
-					state <= 3'b101;
-					ack_o <= 1;
-					dat_o <= ram1[raddr];
-				end
-				3'b101: begin
-					state <= 3'b110;
-				end
-				3'b110: begin
-					ack_o <= 0;
-					state <= 3'b000;
-				end
-				default: begin
-					state <= 3'b000;
-				end
-			endcase
+		if (stb_i) begin
+			if (we_i & sel_i[0]) begin
+				ram0[adr_i] <= dat_i[7:0];
+				ram1[adr_i] <= dat_i[7:0];
+			end else if (sel_i[0]) begin
+				dat_o[7:0] <= ram1[adr_i];
+			end
 		end
 	end
 endmodule
