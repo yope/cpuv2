2f41e41ffa46f4de4adbf8e0d42fca4f185e6733
David Jander <djander@gmail.com>
2021-02-03 18:06:36 +0100

    Build and simulate mac.v
    Signed-off-by: David Jander <djander@gmail.com>

--- Makefile
+++ Makefile
@@ -14,7 +14,7 @@ ulx3s_out.config: cpuv2.json
 		--lpf ulx3s_v20.lpf \
 		--textcfg ulx3s_out.config
 
-cpuv2.json: cpuv2.ys top.v alu.v cpu.v video.v uart.v enet/clk_25_200_100_20.v enet/pls.v firmware.hex
+cpuv2.json: cpuv2.ys top.v alu.v cpu.v video.v uart.v enet/clk_25_200_100_20.v enet/pls.v enet/mac.v firmware.hex
 	yosys cpuv2.ys
 
 firmware.hex: monitor.s
@@ -23,5 +23,8 @@ firmware.hex: monitor.s
 prog: ulx3s.bit
 	fujprog ulx3s.bit
 
-sim: top.v alu.v cpu.v video.v firmware.hex
+sim: top.v alu.v cpu.v video.v uart.v enet/clk_25_200_100_20.v enet/pls.v enet/mac.v firmware.hex
 	python3 simtop.py
+
+plot: sim
+	gtkwave cpuv2sim.vcd
--- simtop.py
+++ simtop.py
@@ -1,16 +1,42 @@
 
+import os
 import pyverilator
 
 class CpuSim:
 	def __init__(self):
 		self.sim = pyverilator.PyVerilator.build('top.v', verilog_path=[".", "hdmi", "enet"])
-		self.sim.start_gtkwave()
-		self.sim.send_to_gtkwave(self.sim.internals)
+		#self.sim.start_gtkwave()
+		#self.sim.send_to_gtkwave(self.sim.internals)
+		self.sim.start_vcd_trace("cpuv2sim.vcd")
+		self.dclkp = [
+				(0, 0),
+				(1, 0),
+				(1, 1),
+				(0, 1),
+				(0, 0),
+				(1, 0),
+				(1, 1),
+				(0, 1),
+				(0, 0),
+				(1, 0),
+				(0, 0),
+				(0, 1),
+				(1, 1),
+				(1, 0),
+				(0, 0),
+				(0, 1),
+				(1, 1)
+			]
+		self.clkidx = 0
 
 	def clock(self):
 		io = self.sim.io
-		io.clk_25mhz = 0
-		io.clk_25mhz = 1
+		cs = self.dclkp[self.clkidx]
+		io.clk_25mhz = cs[0]
+		io.clk_20mhz = cs[1]
+		self.clkidx += 1
+		if self.clkidx >= len(self.dclkp):
+			self.clkidx = 0
 
 	def reset(self):
 		self.sim.io.btn = 0x00
@@ -33,4 +59,4 @@ if __name__ == "__main__":
 	#alu = AluSim()
 	#alu.run()
 	cpu = CpuSim()
-	cpu.run(380)
+	cpu.run(2000)
