e1ad004ff5b415f67cbcf3d607c4b4bd025bfa93
David Jander <djander@gmail.com>
2021-02-10 08:32:32 +0100

    enet: pls.v: Simplify and clean up the state machine a bit
    Signed-off-by: David Jander <djander@gmail.com>

--- enet/pls.v
+++ enet/pls.v
@@ -11,7 +11,7 @@ module pls(
 	output txd_out_n,
 	output txbusy
 );
-	reg [28:0] txcounter;
+	reg [19:0] txcounter;
 	reg [2:0] txstate;
 	reg lit, txd, txen;
 
@@ -19,52 +19,48 @@ module pls(
 
 	always @(posedge clk_20mhz) begin
 		if (rst_i) begin
-			txcounter <= 29'b0;
+			txcounter <= 20'b0;
 			txstate <= 3'b000;
+			txen <= 1'b0;
 		end else begin
 			case (txstate)
 				3'b000: begin
 					// Idle
-					txen <= 1'b0;
-					if (txcounter >= 29'd320000) begin
-						txcounter <= 29'b0;
+					if (txcounter >= 20'd320000) begin
+						txcounter <= 20'b0;
 						lit <= 1'b1;
 					end else if (data_enable) begin
-						txcounter <= 29'b0;
-						txstate <= 3'b001;
+						txcounter <= 20'b0;
+						txstate <= 3'b010;
 						txen <= 1'b1;
 						txd <= !txd_in;
 						lit <= 1'b0;
 					end else begin
 						txcounter <= txcounter + 1;
+						txen <= 1'b0;
 						lit <= 1'b0;
 					end
 				end
 				3'b001: begin
-					// Clocking data
-					if (txcounter[0]) begin
-						txen <= 1'b1;
-						if (data_enable)
-							txd <= !txd_in;
-						else
-							txd <= 1'b1;
-						txcounter <= txcounter + 1;
+					// Clocking data, first half bit
+					if (data_enable) begin
+						txd <= !txd_in;
+						txstate <= 3'b010;
 					end else begin
-						if (data_enable) begin
-							txd <= txd_in;
-							txcounter <= txcounter + 1;
-						end else begin
-							txd <= 1'b1;
-							txstate <= 3'b010;
-							txcounter <= 29'b0;
-						end
+						txd <= 1'b1;
+						txstate <= 3'b011;
 					end
 				end
 				3'b010: begin
+					// Clocking data, second half bit
+					txd <= txd_in;
+					txstate <= 3'b001;
+				end
+				3'b011: begin
 					// 6 bit IDL as etd.
-					if (txcounter >= 29'b1011) begin
-						txstate <= 3'b011;
-						txcounter <= 29'b0;
+					if (txcounter >= 20'b1011) begin
+						txstate <= 3'b100;
+						txcounter <= 20'b0;
 						txd <= 1'b1;
 						txen <= 1'b0;
 					end else begin
@@ -72,7 +68,7 @@ module pls(
 						txd <= 1'b1;
 					end
 				end
-				3'b011: begin
+				3'b100: begin
 					//  Mandatory silence
 					if (txcounter >= 47) begin
 						// Let txcounter continue in idle state
